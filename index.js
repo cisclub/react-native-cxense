db6234e9a5d48d46f14f82bec1e5e2763d652213
aliamin <aamin@gulfnews.com>
2020-07-20 14:54:24 +0400

    fix in index.js

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ const CxenseModule = {
      * @param extraParameter    extra property you can add.
      * @param callbackHandler   (callback) = > {} function called after initialisation is done. if callback equals to NULL/Undefined this means initialisation was successful otherwise it will contain the error.
      */
-    trackEvent(name, siteID, location, profileParameterKey, profileParameterValue, customParameterKey, customParameterValue, extraParameterKey, extraParameterValue, callBackHandler) {
-        Cxense.trackEventWithName(name, siteID, location, profileParameterKey, profileParameterValue, customParameterKey, customParameterValue, extraParameterKey, extraParameterValue, callBackHandler);
-    }
+    trackEvent(name, siteID, callBackHandler, location = null, profileParameterKey = null, profileParameterValue = null, customParameterKey = null, customParameterValue = null, extraParameterKey = null, extraParameterValue = null) {
+        Cxense.trackEventWithName(name, siteID, callBackHandler, location, profileParameterKey, profileParameterValue, customParameterKey, customParameterValue, extraParameterKey, extraParameterValue);
+    },
 
     /*
      * Will force send all events in queue. By default events are sent in batches.
